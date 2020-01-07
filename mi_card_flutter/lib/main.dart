0c85f31d4cbb8319b62dafe9dfa9301124b23fe5
dgtlmonk <dgtlmonk@gmail.com>
2020-01-07 19:50:59 +0800

    :recycle: ListTile

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -16,13 +16,13 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-//    final stageWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Container(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                   radius: 50,
@@ -51,56 +51,28 @@ class Home extends StatelessWidget {
                 ),
                 Card(
                   color: Colors.white,
-//                  width: stageWidth * .75,
-
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-//                  margin: EdgeInsets.all(10),
-//                  padding: EdgeInsets.all(10),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                          child: Icon(
-                            Icons.call,
-                            size: 24,
-                            color: Colors.blueAccent,
-                          ),
-                        ),
-                        Text('+63 919 455 0938')
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.start,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call,
+                      size: 24,
+                      color: Colors.blueAccent,
                     ),
+                    title: Text('+63 919 455 0938'),
                   ),
                 ),
                 Card(
                   color: Colors.white,
-//                  width: stageWidth * .75,
                   margin: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                          child: Icon(
-                            Icons.email,
-                            color: Colors.blue,
-                            size: 24,
-                          ),
-                        ),
-                        Text('hello@dgtlmonk.dev')
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.start,
-                    ),
-                  ),
-                )
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.blue,
+                        size: 24,
+                      ),
+                      title: Text('hello@dgtlmonk.dev')),
+                ),
               ],
-              // alignItems
-              mainAxisAlignment: MainAxisAlignment.center,
-              // flexGrow
-//              crossAxisAlignment: CrossAxisAlignment.stretch,
             ),
             alignment: Alignment(0.0, 0.0),
           ),
@@ -109,45 +81,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
-//
-//
-//p      home: Scaffold(
-//appBar: AppBar(
-//backgroundColor: Colors.grey,
-//title: Text("Demo App"),
-//),
-//backgroundColor: Colors.white,
-//body: SafeArea(
-//child: Container(
-//color: Colors.blueGrey,
-//child: Column(
-//children: <Widget>[
-//Container(
-//color: Colors.white,
-//padding: EdgeInsets.all(10),
-//margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-//child: Text("hey"),
-//),
-//Container(
-//color: Colors.white,
-//padding: EdgeInsets.all(10),
-//margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-//child: Text("hey"),
-//),
-//Container(
-//color: Colors.white,
-//padding: EdgeInsets.all(10),
-//margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-//child: Text("hey"),
-//),
-//],
-//// alignItems
-//mainAxisAlignment: MainAxisAlignment.center,
-//// flexGrow
-//crossAxisAlignment: CrossAxisAlignment.stretch,
-//),
-//alignment: Alignment(0.0, 0.0),
-//),
-//),
-//
