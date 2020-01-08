e7533d4fcaa0e38c731d776c7b3f05d918d42b09
dgtlmonk <dgtlmonk@gmail.com>
2020-01-08 19:04:12 +0800

    :triangular_flag_on_post: Xylophone - playing audio files

--- xylophone/README.md
+++ xylophone/README.md
@@ -9,10 +9,12 @@ In this tutorial we will be diving into more Dart programming concepts and learn
 
 
 ## What you will create
+We’re going to make a music app that plays Xylophone sounds. For every Beethoven out there, this will let you unleash your musical talent where ever you are.
 
-We’re going to make a music app that plays Xylophone sounds. For every Beethoven out there, this will let you unleash your musical talent where ever you are. 
 
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/xylophone-flutter.png)
+## Finished oOutput
+<img src="assets/xylophone.png" alt="xylophone" width="320"/>
+
 
 ## What you will learn
 
--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,14 +1,43 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final List<Color> colors = [
+    Colors.blue,
+    Colors.blueGrey,
+    Colors.orange,
+    Colors.lightGreen,
+    Colors.amberAccent,
+    Colors.lightGreen,
+    Colors.greenAccent,
+    Colors.purple,
+  ];
+
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(children: <Widget>[
+            for (var i = 1; i < colors.length; i++)
+              Expanded(
+                child: Container(
+                  color: colors[i - 1],
+                  width: 200,
+                  child: FlatButton(
+                    highlightColor: Colors.transparent,
+                    onPressed: () {
+                      player.play('note$i.wav');
+                    },
+                    child: Text('Play Sound'),
+                  ),
+                ),
+              )
+          ], crossAxisAlignment: CrossAxisAlignment.stretch),
         ),
       ),
     );
