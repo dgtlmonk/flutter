8c2399e48e38c584d709e73244b9a13a2208ff11
dgtlmonk <dgtlmonk@gmail.com>
2020-01-10 14:19:03 +0800

    :recycle: refactor bmi calc

--- bmi-calculator/lib/components/BmiInput.dart
+++ bmi-calculator/lib/components/BmiInput.dart
@@ -15,150 +15,159 @@ class _BmiInputViewState extends State<BmiInputView> {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Container(
-                child: Expanded(
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1C1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Icon(
+                                Icons.beach_access,
+                                size: 64,
+                              ),
+                              Text(
+                                'FEMALE',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700, fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1C1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Icon(
+                                Icons.beach_access,
+                                size: 64,
+                              ),
+                              Text(
+                                'MALE',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700, fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1C1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  margin: EdgeInsets.all(10),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Container(
-                          child: Row(
-                        children: <Widget>[
-                          Container(
-                            child: Expanded(
-                              child: Container(
-                                height: 150,
-                                margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF1C1E33),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.beach_access,
-                                      size: 64,
-                                    ),
-                                    Text(
-                                      'FEMALE',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 20),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: Expanded(
-                              child: Container(
-                                height: 150,
-                                margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF1C1E33),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.beach_access,
-                                      size: 64,
-                                    ),
-                                    Text(
-                                      'MALE',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 20),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )),
+                      Text(
+                        'HEIGHT',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 22,
+                        ),
+                      ),
                       Container(
                         child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            Container(
-                              child: Expanded(
-                                child: Container(
-                                    padding: EdgeInsets.all(30),
-                                    child: Column(
-                                      children: <Widget>[
-                                        Text(
-                                          'HEIGHT',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 22,
-                                          ),
-                                        ),
-                                        Container(
-                                          child: Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.end,
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: <Widget>[
-                                              Text(
-                                                '183',
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.w700,
-                                                  fontSize: 60,
-                                                ),
-                                              ),
-                                              Text(
-                                                'cm',
-                                              )
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    )),
+                            Text(
+                              '183',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 60,
+                              ),
+                            ),
+                            Text(
+                              'cm',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w700,
                               ),
                             )
                           ],
                         ),
-                        margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF1C1E33),
-                        ),
-                      ),
-                      Container(
-                          child: Row(
-                        children: <Widget>[
-                          Container(
-                            child: Expanded(
-                              child: Container(
-                                margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF1C1E33),
-                                ),
-                                child: Text('hey'),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: Expanded(
-                              child: Container(
-                                margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF1C1E33),
-                                ),
-                                child: Text('hey'),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )),
-
-                      // Calculate Button
+                      )
                     ],
                   ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1C1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Icon(
+                                Icons.beach_access,
+                                size: 64,
+                              ),
+                              Text(
+                                'FEMALE',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700, fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1C1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Icon(
+                                Icons.beach_access,
+                                size: 64,
+                              ),
+                              Text(
+                                'MALE',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700, fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Container(
