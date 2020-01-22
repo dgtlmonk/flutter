ba45d49fe5b22ae10377522b06b9b3c68463a2fd
dgtlmonk <dgtlmonk@gmail.com>
2020-01-22 21:02:41 +0800

    :wrench: add ios/android configs and api keys

--- WeatherApp/ios/Runner/AppDelegate.swift
+++ WeatherApp/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCqjCwVbmyf-LSkdG7g5u6jcQ5DUojz-go")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
